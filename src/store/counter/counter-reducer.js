4f24475c0bc2a49b94166e27894b1d5d38962b0a
Ersin Akun <ersinakun34@gmail.com>
2023-01-19 22:56:49 +0300

    v2

--- src/store/counter/counter-reducer.js
+++ src/store/counter/counter-reducer.js
@@ -1,31 +1,27 @@
-import { COUNTER_DOWN, COUNTER_UP,  COUNTER_SET} from "../types";
+import { COUNTER_DOWN, COUNTER_SET, COUNTER_UP } from "../types";
 import { counterInitialState } from "./counter-initial-state";
 
 
+export const counterReducer = (state = counterInitialState, action) => {
+
+  if (action.type === COUNTER_UP) {
+    const newState = { ...state, counter: state.counter + 1 };
 
 
-export const counterReducer = (state = counterInitialState, action) => { 
-
-    if(action.type === COUNTER_UP){
-        const newState = {...state, counter: state.counter+1} 
-        return newState;
-
-    }
-    else if(action.type === COUNTER_DOWN){
-
-    }
-    else if(action.type === COUNTER_SET){
-
-    }
-
-    //Bu satir hic bir if case ine girmediginde geriye mevcut state i gondersin diye yazildi
-    //Eger olmazsa tum state ucar.
-    return state;
-
- }
+    // Oluşturulan yeni state return edildiğinde mevcut state güncellenmiş olur.
+    return newState;
 
 
+  } else if (action.type === COUNTER_DOWN) {
 
+    
+  } else if (action.type === COUNTER_SET) {
+  }
+
+  // Bu satır hiç bir if case ine girmediğinde geriye mevcut state i göndersin diye yazıldı
+  // Eğer olmazsa tüm state uçar.
+  return state;
+};
 
 
 
