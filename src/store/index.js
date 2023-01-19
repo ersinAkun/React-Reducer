b1345bb65cd2d018fd9015ebed157a5329f651da
Ersin Akun <ersinakun34@gmail.com>
2023-01-19 23:36:06 +0300

    v5

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,6 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
+import { counterInitialState } from "./counter/counter-initial-state";
+import { counterReducer } from "./counter/counter-reducer";
 
 
 // Bos bir merkezi state olusturuldu
@@ -10,6 +12,11 @@ export const useStore = () => useContext(StoreContext);
 
 
 export const StoreProvider = ({children}) => {
+        // useReducer hook una reducer ve initialstate parametre olarak verilir...
+        // useReducer fonksiyonu ise geriye setter ve getter lari doner.
+        //     getter         setter    
+    const [counterState, dispatchCounter] = useReducer(counterReducer, counterInitialState); 
+
 
 
     return(
