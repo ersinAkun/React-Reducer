377023b39f0c194c31f4ea2582f6425ee4aec594
Ersin Akun <ersinakun34@gmail.com>
2023-01-20 23:11:56 +0300

    v3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import HomePage from './pages/home-page'
+import AppRouter from './router'
 import { StoreProvider } from './store'
+
 
 const App = () => {
   
   return (
     <StoreProvider>
-      <HomePage/>
+      <AppRouter/>
     </StoreProvider>
   )
 }
